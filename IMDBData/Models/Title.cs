ffc1f7d7cec472dd6dbd5919621ca2ba5cb13ab1
Nuggitt <Phulip.nielsen@gmail.com>
2024-09-19 12:23:09 +0200

    Add project files.

---
+++ IMDBData/Models/Title.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBData.Models
+{
+    public class Title
+    {
+        public string Tconst { get; set; }
+        public string PrimaryTitle  { get; set; }
+        public string OriginalTitle { get; set; }
+        public bool IsAdult { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+        public int RunTimeMinutes { get; set; }
+    }
+}
---
+++ IMDBData/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int lineCount = 0;
+string filePath = "C:/IMDBData/title.basics.tsv";
+
+foreach (string line in File.ReadLines(filePath))
+{
+    Console.WriteLine(line);
+
+    if (lineCount++ == 50000)
+    {
+        break;
+    }
+
+    string[] splitLine = line.Split("\t");
+    if (splitLine.Length != 9)
+    {
+        throw new Exception("Invalid line: " + line);
+    }
+    string tconst = splitLine[0];
+    string primaryTitle = splitLine[1];
+    string originalTitle = splitLine[2];
+    bool isAdult = splitLine[3] == "1";
+
+    lineCount++;
+}
